b7bd13d45afd154d29028d9b89ad72d3fed17069
Marshall Pierce <marshall@palominolabs.com>
2013-03-07 12:04:29 -0800

    Make the benchmark a little clearer

--- src/main/java/com/palominolabs/staxmate/meat/BenchmarkMain.java
+++ src/main/java/com/palominolabs/staxmate/meat/BenchmarkMain.java
@@ -8,43 +8,48 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.XMLStreamException;
 import java.io.IOException;
 
-final class PerformanceTestMain {
+final class BenchmarkMain {
+
+    private static final int ITERATIONS = 1000000;
+
     public static void main(String[] args) throws ParserConfigurationException, XMLStreamException, IOException,
         SAXException {
         MeatXmlParser parser = new MeatXmlParser();
 
+        System.out.println("This benchmark is very simple. If you need real benchmarking, use something like Caliper.");
+
         System.out.println("Running stax");
 
-        for (int i = 0; i < 100000; i++) {
-            parser.parse(PerformanceTestMain.class.getResourceAsStream("/sample.xml"));
+        for (int i = 0; i < ITERATIONS; i++) {
+            parser.parse(BenchmarkMain.class.getResourceAsStream("/sample.xml"));
         }
 
         long staxStart = System.nanoTime();
 
-        for (int i = 0; i < 100000; i++) {
-            parser.parse(PerformanceTestMain.class.getResourceAsStream("/sample.xml"));
+        for (int i = 0; i < ITERATIONS; i++) {
+            parser.parse(BenchmarkMain.class.getResourceAsStream("/sample.xml"));
         }
         long staxEnd = System.nanoTime();
 
-        System.out.println("Stax millis: " + (staxEnd - staxStart) / 1000000);
+        System.out.println("Stax ns per iteration: " + (staxEnd - staxStart) / ITERATIONS);
 
         System.out.println("Running dom");
 
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
 
-        for (int i = 0; i < 100000; i++) {
-            db.parse(PerformanceTestMain.class.getResourceAsStream("/sample.xml"));
+        for (int i = 0; i < ITERATIONS; i++) {
+            db.parse(BenchmarkMain.class.getResourceAsStream("/sample.xml"));
         }
 
         long domStart = System.nanoTime();
 
-        for (int i = 0; i < 100000; i++) {
-            db.parse(PerformanceTestMain.class.getResourceAsStream("/sample.xml"));
+        for (int i = 0; i < ITERATIONS; i++) {
+            db.parse(BenchmarkMain.class.getResourceAsStream("/sample.xml"));
         }
 
         long domEnd = System.nanoTime();
 
-        System.out.println("Dom millis: " + (domEnd - domStart) / 1000000);
+        System.out.println("Dom ns per iteration: " + (domEnd - domStart) / ITERATIONS);
     }
 }
